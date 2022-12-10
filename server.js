489f40d08c51a42915ab3ae6c7a0813547429536
kevinbazira <kvnbzr@gmail.com>
2022-12-10 11:12:12 +0400

    added express middleware for '/lessons' get route

--- server.js
+++ server.js
@@ -2,6 +2,17 @@ const express = require("express");
 
 const app = express();
 
+// get route for "/lessons" path that returns lessons JSON object
+app.get("/lessons", (request, response, next)=>{
+    const lessons = [
+        {'topic': 'math', 'location': 'Hendon', 'price' : 100},
+        {'topic': 'math', 'location': 'Colindale', 'price' : 80},
+        {'topic': 'math', 'location': 'Brent Cross', 'price' : 90},
+        {'topic': 'math', 'location': 'Golders Green', 'price' : 120},
+    ];
+    response.send(lessons);
+});
+
 app.listen(3000, ()=>{
     console.log("App started on 3000 ");
 });
